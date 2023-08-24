1bfc557c7f360027c430215f2b57fc9037c8f9e6
rodrigo-silva-new <142907639+rodrigo-silva-new@users.noreply.github.com>
2023-08-24 21:28:28 +0000

    mudando a frase para fazer um novo deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Esta é a frase definitiva para esse teste (mais uma mudança)</h1>
+    return <h1>Nova frase, só para criar um novo deploy</h1>
 }
 
 export default Home;
