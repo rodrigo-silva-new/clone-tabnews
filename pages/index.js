a01422b159d73ec61f711f058caa3843a88aaed8
rodrigo-silva-new <142907639+rodrigo-silva-new@users.noreply.github.com>
2023-09-07 20:41:34 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Você consegue ler essa frase? Agora me deve 10 milhões de dólares!</h1>
+  return (
+    <h1>Você consegue ler essa frase? Agora me deve 10 milhões de dólares!</h1>
+  );
 }
 
 export default Home;
